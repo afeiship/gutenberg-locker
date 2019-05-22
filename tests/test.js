dbdd309ac12fc6c9887091eaa8079e897154edc0
afeiship <1290657123@qq.com>
2019-05-22 17:13:19 +0800

    add: test json file

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 var gtbLocker = require('../index');
 var fs = require('fs');
-var files = gtbLocker('node_modules/@wordpress/**/package.json');
+var files1 = gtbLocker({ path: 'node_modules/@wordpress/**/package.json' , key: '_id'});
+var files2 = gtbLocker({ path: 'node_modules/@wordpress/**/package.json' , key: '_from'});
 
-fs.writeFileSync('./tests/pkg.json', JSON.stringify(files, null, 2));
+fs.writeFileSync('./tests/pkg_id.json', JSON.stringify(files1, null, 2));
+fs.writeFileSync('./tests/pkg_from.json', JSON.stringify(files2, null, 2));
